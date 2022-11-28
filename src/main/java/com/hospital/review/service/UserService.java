59e3710f27be36d364dd09053411505485827aa8
S2ujeong <sj980204@gmail.com>
2022-11-28 14:18:23 +0900

    - 회원가입, 로그인 기능  - 오류 : null 값이 뜬다.

--- src/main/java/com/hospital/review/service/UserService.java
+++ src/main/java/com/hospital/review/service/UserService.java
@@ -1,13 +1,33 @@
 package com.hospital.review.service;
 
+import com.hospital.review.domain.User;
 import com.hospital.review.domain.dto.UserDto;
 import com.hospital.review.domain.dto.UserJoinRequest;
+import com.hospital.review.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    // userName, password를 받아서 회원가입 처리
+    private final UserRepository userRepository;
+
     public UserDto join(UserJoinRequest request) {
-        return new UserDto("","","");
+        // 비즈니스 로직 - 회원가입
+
+        // 회원 userName(id) 중복 check
+        // 중복이면  회원가입 x --> Exception(예외)발생
+        userRepository.findByUserName(request.getUserName())
+                .ifPresent(user -> {
+                    throw new RuntimeException("해당 UserName이 중복됩니다.");
+                });
+
+         // 중복 아니면 회원가입 .save()
+        User savedUser = userRepository.save(request.toEntity());
+        return UserDto.builder()
+                .id(savedUser.getId())
+                .userName(savedUser.getUserName())
+                .email(savedUser.getEmailAddress())
+                .build();
     }
 }
